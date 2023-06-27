55278fbe856b48bb7018d15bb7f70452c08227e7
Kiskotkd <fjgp92@hotmail.com>
2023-06-27 10:22:05 +0200

    añadir reto2

---
+++ Reto2dia5.js
@@ -0,0 +1,20 @@
+function suma(x, y){
+    console.log(x+y)
+};
+
+function resta(x, y){
+    console.log(x-y)
+};
+
+function producto(x, y){
+    console.log(x*y)
+};
+
+function div(x, y){
+    console.log(x/y)
+};
+
+suma();
+resta();
+producto();
+div();
